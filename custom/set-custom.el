f17bb744b91374296485e8dc145ac734c3a1c035
Ed Maphis <emaphis85@gmail.com>
2016-05-19 15:03:04 -0400

    Update set-custom

---
+++ custom/set-custom.el
@@ -0,0 +1,16 @@
+;;; set-custom -- Emacs customizations
+;; Copyright © 2016 Ed Maphis
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+
+
+(message "end set-custom.el")
+
+(provide 'set-custom)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; set-custom.el ends here
