01245b868c144dd8117573a81bf8797147a98da9
emaphis <emaphis85@gmail.com>
2016-05-19 15:23:20 -0400

    Add code to set-custom

--- custom/set-custom.el
+++ custom/set-custom.el
@@ -6,6 +6,20 @@
 ;;; Code:
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 120 :width normal)))))
 
 
 
