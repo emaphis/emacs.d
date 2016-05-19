40220c9e60dd44e7db13b72892e9673d996bd3d3
Ed Maphis <emaphis85@gmail.com>
2016-05-19 14:37:38 -0400

    Switch Melpa repositories

--- init.el
+++ init.el
@@ -56,10 +56,10 @@
 ;;;
 (require 'package)
 
-;;(add-to-list 'package-archives
-;;             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
