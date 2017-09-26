86bbc963bd27ced26f4ec5783fa225f956fe33fa
Ed Maphis <emaphis85@gmail.com>
2017-09-26 15:36:41 -0400

    Update set-template.el with more commentary.

---
+++ custom/set-template.el
@@ -0,0 +1,12 @@
+;;; template.el -- An Elisp file template
+
+;;; Commentary:
+;;  A template file that follows Elisp conventions.
+;;
+
+;;; Code:
+
+
+
+(provide 'set-template)
+;;; set-template.el ends here
